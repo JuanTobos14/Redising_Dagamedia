b651c11be5488822750116295936ad6621486f4c
unknown <sdrodriguezm@uniboyaca.edu.co>
2026-05-15 15:14:26 -0500

    feat: implementacion de rutas y navegacion multi-pagina

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,27 @@
-// src/App.jsx
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Header from './components/Header/Header';
+import Footer from './components/Footer/Footer';
+import Home from './pages/Home/Home';
 import AboutUs from './components/AboutUs/AboutUs';
-import WhatDoWeDo from './components/WhatDoWeDo/WhatDoWeDo'; // Asegúrate de que el archivo se llame igual que la carpeta
+import WhatDoWeDo from './components/WhatDoWeDo/WhatDoWeDo';
 import OurFilms from './components/Ourfilms/OurFilms';
 import Contact from './components/Contact/Contact';
 
 function App() {
   return (
-    <main style={{ backgroundColor: '#000', color: '#fff' }}>
-      <AboutUs />
-      <WhatDoWeDo />
-      <OurFilms />
-      <Contact />
-    </main>
+    <Router>
+      <Header />
+      <main className="content">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about-us" element={<AboutUs />} />
+          <Route path="/what-we-do" element={<WhatDoWeDo />} />
+          <Route path="/our-films" element={<OurFilms />} />
+          <Route path="/contact" element={<Contact />} />
+        </Routes>
+      </main>
+      <Footer />
+    </Router>
   );
 }
 
