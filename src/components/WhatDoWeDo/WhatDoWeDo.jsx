96ab108b0f74447e0d6c9053221dc99a12342101
CamiloAlarconA <edgalarcon@uniboyaca.edu.co>
2026-05-22 23:01:31 -0500

    Creación componente WhatDoWeDo

---
+++ src/components/WhatDoWeDo/WhatDoWeDo.jsx
@@ -0,0 +1,12 @@
+const WhatDoWeDo = () => {
+  return (
+    <section>
+      <h2>What Do We Do?</h2>
+      <p>
+        We provide solutions to improve your business through technology.
+      </p>
+    </section>
+  );
+};
+
+export default WhatDoWeDo;
